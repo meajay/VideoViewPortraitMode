c801efa8a8616b6dbcecb95fa14d08a34a26045c
Ajay Chauhan <achauhan142@gmail.com>
2018-03-16 16:43:58 +0530

    First Commit

---
+++ app/src/main/java/testme/java/com/videoplayerfullscreen/presenter/MainMvpPresenter.java
@@ -0,0 +1,8 @@
+package testme.java.com.videoplayerfullscreen.presenter;
+
+/**
+ * Created by Ajay on 16-03-2018.
+ */
+
+public interface MainMvpPresenter {
+}
---
+++ app/src/main/java/testme/java/com/videoplayerfullscreen/view/VideoMvpView.java
@@ -0,0 +1,8 @@
+package testme.java.com.videoplayerfullscreen.view;
+
+/**
+ * Created by Ajay on 16-03-2018.
+ */
+
+public interface VideoMvpView {
+}
